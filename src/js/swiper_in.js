4a7925b921d187a9beddc5a5f848b278e92a02a0
SavchenkoVolodymyr <v1987savchenko@ukr.net>
2024-11-29 23:40:50 +0200

    swip

--- src/js/swiper_in.js
+++ src/js/swiper_in.js
@@ -1,10 +1,26 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.trust_pic_swiper', {
 	// Optional parameters
 	direction: 'horizontal',
 	loop: true,
-	// Navigation arrows
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
+	centeredSlides: true,
+	centeredSlides: true,
+	autoplay: {
+	  delay: 1500,
+	  disableOnInteraction: false,
+	},
+	
+  });
+
+const swiperTwo = new Swiper('.comments_nav', {
+	slidesPerView: 5,
+	loop: true,
+
+});
+
+const swiperOne = new Swiper('.comments_content', {
+	initialSlide: 2,
+	thumbs: {
+		swiper: swiperTwo,
 	},
 });
+
