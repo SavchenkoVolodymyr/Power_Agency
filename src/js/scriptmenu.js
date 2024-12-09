ef6ee627bec9b5284ba343e794b712c3c9023ebf
SavchenkoVolodymyr <v1987savchenko@ukr.net>
2024-12-09 14:55:41 +0200

    feat:resp$

--- src/js/scriptmenu.js
+++ src/js/scriptmenu.js
@@ -4,6 +4,7 @@ const menuButton = document.querySelector('.menu_button');
 const menuCloseButton = document.querySelector('.nav_close');
 const menu = document.querySelector('.nav');
 const bodyK = document.querySelector('.body');
+const navLink = document.querySelector('.nav_list__link')
 
 menuButton.addEventListener('click', function () {
 	menu.classList.add('active');
@@ -11,6 +12,11 @@ menuButton.addEventListener('click', function () {
 });
 
 menuCloseButton.addEventListener('click', function () {
+	menu.classList.remove('active');
+	bodyK.classList.remove('body--overflow_hidden');
+});
+
+navLink.addEventListener('click', function () {
 	menu.classList.remove('active');
 	bodyK.classList.remove('body--overflow_hidden');
 });
